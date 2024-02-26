f2b6e4e71d6d51b0bfc1e220910e13eea36ee893
Anuraaaa <anwarfauzan75@gmail.com>
2024-02-26 15:38:32 +0700

    add javascript testing, jest

---
+++ Javascripts/JavascriptTesting/_example_test_/example.js
@@ -0,0 +1,8 @@
+test('deskripsi dari testcase kamu', () => {
+    expect(perintah).matcher(nilaiEkspektasi);
+});
+ 
+// contoh
+test('dua tambah dua adalah empat', () => {
+    expect(2+2).toBe(4);
+});
