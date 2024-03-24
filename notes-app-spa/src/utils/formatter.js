80c9c4015aa8a3c403031c033aa6587905718f0b
Anuraaaa <anwarfauzan75@gmail.com>
2024-03-24 16:31:59 +0700

    other

--- notes-app-spa/src/utils/formatter.js
+++ notes-app-spa/src/utils/formatter.js
@@ -1,22 +1,19 @@
 const formatDate = (dateString) => {
     const date = new Date(dateString);
     
-    // Extract date components
     const day = date.getDate();
-    const month = date.getMonth() + 1; // Month is zero-based
+    const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     
-    // Pad single digit values with leading zeros
     const formattedDay = day < 10 ? '0' + day : day;
     const formattedMonth = month < 10 ? '0' + month : month;
     const formattedHours = hours < 10 ? '0' + hours : hours;
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
     const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
     
-    // Construct formatted date string
     const formattedDate = `${formattedDay}/${formattedMonth}/${year} ${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
     
     return formattedDate;
