cc627985da1b5f2b0bdde6c08245b5e13bfcf99b
Anuraaaa <anwarfauzan75@gmail.com>
2024-04-04 23:25:18 +0700

    implement switch language toast message in login and register page

--- notes-app-spa-v2/src/pages/Loginpage.jsx
+++ notes-app-spa-v2/src/pages/Loginpage.jsx
@@ -7,7 +7,6 @@ import PropTypes from "prop-types";
 import { useLocaleProvider } from "../components/LocaleProvider";
 
 function Loginpage({ loginSuccess }) {
-
     const [email, onEmailChange] = useInput('');
     const [password, onPasswordChange] = useInput('');
     const { language } = useLocaleProvider();
@@ -17,30 +16,30 @@ function Loginpage({ loginSuccess }) {
     const handleSubmit = async (event) => {
         event.preventDefault();
     
-        if (email.length == 0)
-            return showToast("Gagal login! email tidak ada", "white", "red");            
+        if (email.length === 0)
+            return showToast(language === 'en' ? 'Failed to login! Email is required' : 'Gagal login! email tidak ada', "white", "red");            
 
         if (email.length > 64)
-            return showToast("Gagal login! email maksimal 64 karakter", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to login! Email maximum 64 characters' : 'Gagal login! email maksimal 64 karakter', "white", "red");            
 
-        if (password.length == 0)
-            return showToast("Gagal login! password tidak ada", "white", "red");            
+        if (password.length === 0)
+            return showToast(language === 'en' ? 'Failed to login! Password is required' : 'Gagal login! password tidak ada', "white", "red");            
 
         if (password.length < 6)
-            return showToast("Gagal login! password minimal 6 karakter", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to login! Password minimum 6 characters' : 'Gagal login! password minimal 6 karakter', "white", "red");            
 
         if (password.length > 255)
-            return showToast("Gagal login! password maksimal 255 karakter", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to login! Password maximum 255 characters' : 'Gagal login! password maksimal 255 karakter', "white", "red");            
 
         const { error, data } = await login({
             email,
             password
         });
         if (error) {
-            showToast("Gagal login", "white", "red");            
+            showToast(language === 'en' ? 'Failed to login' : 'Gagal login', "white", "red");            
         } else {            
             loginSuccess(data);
-            showToast("Berhasil login", "black", "rgb(0, 204, 255)"); 
+            showToast(language === 'en' ? 'Login successful' : 'Berhasil login', "black", "rgb(0, 204, 255)"); 
             navigate("/");           
         }
     };
@@ -57,7 +56,7 @@ function Loginpage({ loginSuccess }) {
                 <input type="password" id="password" value={password} onChange={onPasswordChange} />
             </div>
             <div className="form-group-row">
-                <Link to={"/register"}>{language == 'en' ? 'Don\'t have an account? Register here' : 'Tidak punya akun? Daftar disini'}</Link>
+                <Link to={"/register"}>{language === 'en' ? 'Don\'t have an account? Register here' : 'Tidak punya akun? Daftar disini'}</Link>
                 <button type="submit">Login</button>
             </div>
         </form>
--- notes-app-spa-v2/src/pages/Registerpage.jsx
+++ notes-app-spa-v2/src/pages/Registerpage.jsx
@@ -6,7 +6,6 @@ import useInput from "../components/UseInput";
 import { useLocaleProvider } from "../components/LocaleProvider";
 
 function Registerpage() {
-
     const [name, onNameChange] = useInput('');
     const [email, onEmailChange] = useInput('');
     const [password, onPasswordChange] = useInput('');
@@ -15,33 +14,32 @@ function Registerpage() {
 
     const navigate = useNavigate();
 
-
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (name.length == 0)
-            return showToast("Gagal register! nama tidak ada", "white", "red");            
-        
+        if (name.length === 0)
+            return showToast(language === 'en' ? 'Failed to register! Name is required' : 'Gagal register! nama tidak ada', "white", "red");            
+
         if (name.length > 16)
-            return showToast("Gagal register! nama maksimal 16 karakter", "white", "red");            
-    
-        if (email.length == 0)
-            return showToast("Gagal register! email tidak ada", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to register! Name maximum 16 characters' : 'Gagal register! nama maksimal 16 karakter', "white", "red");            
+
+        if (email.length === 0)
+            return showToast(language === 'en' ? 'Failed to register! Email is required' : 'Gagal register! email tidak ada', "white", "red");            
 
         if (email.length > 64)
-            return showToast("Gagal register! email maksimal 64 karakter", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to register! Email maximum 64 characters' : 'Gagal register! email maksimal 64 karakter', "white", "red");            
 
-        if (password.length == 0)
-            return showToast("Gagal register! password tidak ada", "white", "red");            
+        if (password.length === 0)
+            return showToast(language === 'en' ? 'Failed to register! Password is required' : 'Gagal register! password tidak ada', "white", "red");            
 
         if (password.length < 6)
-            return showToast("Gagal register! password minimal 6 karakter", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to register! Password minimum 6 characters' : 'Gagal register! password minimal 6 karakter', "white", "red");            
 
         if (password.length > 255)
-            return showToast("Gagal register! password maksimal 255 karakter", "white", "red");            
+            return showToast(language === 'en' ? 'Failed to register! Password maximum 255 characters' : 'Gagal register! password maksimal 255 karakter', "white", "red");            
 
-        if (password != confpassword)
-            return showToast("Gagal register! konfirmasi password tidak sama", "white", "red");            
+        if (password !== confpassword)
+            return showToast(language === 'en' ? 'Failed to register! Confirm password does not match' : 'Gagal register! konfirmasi password tidak sama', "white", "red");            
 
         const { error } = await register({
             name,
@@ -49,9 +47,9 @@ function Registerpage() {
             password
         });
         if (error) {
-            showToast("Gagal register", "white", "red");            
+            showToast(language === 'en' ? 'Failed to register' : 'Gagal register', "white", "red");            
         } else {            
-            showToast("Berhasil register", "black", "rgb(0, 204, 255)"); 
+            showToast(language === 'en' ? 'Registration successful' : 'Berhasil register', "black", "rgb(0, 204, 255)"); 
             navigate("/login");           
         }
     };
@@ -76,7 +74,7 @@ function Registerpage() {
                 <input type="password" id="confpassword" value={confpassword} onChange={onConfPasswordChange} />
             </div>
             <div className="form-group-row">
-                <Link to={"/login"}>{language == 'en' ? 'Already have an account? Login here' : 'Sudah punya akun? Login disini'}</Link>
+                <Link to={"/login"}>{language === 'en' ? 'Already have an account? Login here' : 'Sudah punya akun? Login disini'}</Link>
                 <button type="submit">Register</button>
             </div>
         </form>
