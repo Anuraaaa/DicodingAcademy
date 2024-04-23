45c279f0208acf6d176ed3e292c3b1b613970b96
Anuraaaa <anwarfauzan75@gmail.com>
2024-04-23 12:10:24 +0700

    initialize forum-app

---
+++ forum-app/src/components/Navigation.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function Navigation() {
+    return (
+        <div className="navbar">
+            <h1>Aplikasi Note</h1>
+            <ul>
+                <li className="dropdown">
+                    <Link className="dropdown-title">
+                        <div>Nama</div>
+                        <div className="material-symbols-outlined">expand_more</div>
+                    </Link>
+                    <div className="dropdown-content">
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/notes/new"}>New</Link>
+                        <Link to={"/notes/archive"}>Archive</Link>
+                        <button>Logout</button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+Navigation.propTypes = {
+    logout: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired
+}
+
+export default Navigation;
