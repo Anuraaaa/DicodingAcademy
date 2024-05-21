4210ba80a9add8b6a0e62299901ba688de4fabc3
Anuraaaa <anwarfauzan75@gmail.com>
2024-05-21 10:39:01 +0700

    initialize, routes, eslint

---
+++ backend-bookshelf/src/routes.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },        
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;            
+        }
+    }
+];
+ 
+module.exports = routes;
